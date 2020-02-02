1cb3db85e8a3780d82e84f8f38b4dd9655ba70bd
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-02 16:50:30 -0500

    Model And View.

--- src/main/java/com/example/dependency/controller/coursesController.java
+++ src/main/java/com/example/dependency/controller/coursesController.java
@@ -3,6 +3,7 @@ package com.example.dependency.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,11 +12,11 @@ import javax.servlet.http.HttpSession;
 public class coursesController {
 
     @RequestMapping("/")
-    public String courses(@RequestParam("cname") String course, HttpSession session){
+    public ModelAndView courses(@RequestParam("cname") String course, HttpSession session){
 
-
-        session.setAttribute( "cname", course);
-        System.out.println("welcome");
-        return "course";
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("cname", course);
+        mv.setViewName("course");
+        return mv;
     }
 }
